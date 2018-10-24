25627df642ebc7c551bd4bde31aa9738e114512f
Jeremy Brown <jeremy.r.brown@gmail.com>
2018-10-24 15:20:24 -0700

    Added dependencies to React; basic router
    npm install --save react-router-dom
    npm install --save axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      	<h2>App Component</h2>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
