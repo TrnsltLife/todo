113f326c08b531e6aa837e54bd1ecb5a6445866a
Jeremy Brown <jeremy.r.brown@gmail.com>
2018-10-24 17:19:01 -0700

    Added trashcan icon to TodoItem
    Doesn't do anything yet

--- frontend/src/components/TodoItem.js
+++ frontend/src/components/TodoItem.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
+import {Glyphicon} from 'react-bootstrap';
 
 class TodoItem extends Component
 {
@@ -16,6 +17,8 @@ class TodoItem extends Component
 				<input name="completed" type="checkbox" checked={this.props.completed} />
 			</td><td>
 				<span>{this.props.text}</span>
+			</td><td>
+				<Glyphicon glyph="trash" />
 			</td></tr>
 		);
 	}
