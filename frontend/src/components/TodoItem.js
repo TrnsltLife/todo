1dcd6c788e4a848b8b286067dcaab64f4f0ae604
Jeremy Brown <jeremy.r.brown@gmail.com>
2018-10-24 23:34:51 -0700

    Add delete functionality to TodoItem

--- frontend/src/components/TodoItem.js
+++ frontend/src/components/TodoItem.js
@@ -8,17 +8,31 @@ class TodoItem extends Component
 	constructor(props)
 	{
 		super(props);
+		this.onClickDelete = this.onClickDelete.bind(this);
+	}
+	
+	onClickDelete(event)
+	{
+		//console.log('DELETE: /todo/' + this.props.todo.id);
+		axios.delete('/todo/' + this.props.todo.id)
+		.then(() => {
+			this.props.onDelete(this.props.todo);
+		});
 	}
 	
 	render()
 	{
+		const paddingStyle = {
+			"padding-right":"5px"
+		}
+		
 		return (
-			<tr><td>
-				<input name="completed" type="checkbox" checked={this.props.completed} />
+			<tr><td style={paddingStyle}>
+				<input name="completed" type="checkbox" checked={this.props.todo.completed} />
+			</td><td style={paddingStyle}>
+				<span>{this.props.todo.text}</span>
 			</td><td>
-				<span>{this.props.text}</span>
-			</td><td>
-				<Glyphicon glyph="trash" />
+				<Glyphicon glyph="trash" onClick={this.onClickDelete} />
 			</td></tr>
 		);
 	}
