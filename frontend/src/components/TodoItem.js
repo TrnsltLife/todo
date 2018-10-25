e5fec305a60434e76ed9d6021035f477ab1a9692
Jeremy Brown <jeremy.r.brown@gmail.com>
2018-10-25 01:17:33 -0700

    Revert "Revert "Changed the initial data""
    This reverts commit f706231b474a39a88446d08bdb6779a507885e81.

--- frontend/src/components/TodoItem.js
+++ frontend/src/components/TodoItem.js
@@ -8,15 +8,29 @@ class TodoItem extends Component
 	constructor(props)
 	{
 		super(props);
+		this.state = {
+			completed: !!this.props.todo.completed
+		}
 		this.onClickDelete = this.onClickDelete.bind(this);
+		this.onChange = this.onChange.bind(this);
 	}
 	
 	onClickDelete(event)
 	{
-		//console.log('DELETE: /todo/' + this.props.todo.id);
 		axios.delete('/todo/' + this.props.todo.id)
 		.then(() => {
 			this.props.onDelete(this.props.todo);
+		});
+	}
+	
+	onChange(event)
+	{
+		const state = this.state;
+		state.completed = event.target.checked;
+		
+		axios.put('/todo/' + this.props.todo.id, {completed:state.completed})
+		.then((result) => {
+			this.setState(state);			
 		});
 	}
 	
@@ -28,9 +42,16 @@ class TodoItem extends Component
 		
 		return (
 			<tr><td style={paddingStyle}>
-				<input name="completed" type="checkbox" checked={this.props.todo.completed} />
+				<input name="completed" type="checkbox" checked={this.state.completed} onChange={this.onChange} />
 			</td><td style={paddingStyle}>
-				<span>{this.props.todo.text}</span>
+				{/*if not completed, display span*/}
+				{!this.state.completed &&
+						<span>{this.props.todo.text}</span>
+				}
+				{/*if completed, display del*/}
+				{this.state.completed &&
+					<del>{this.props.todo.text}</del>
+				}
 			</td><td>
 				<Glyphicon glyph="trash" onClick={this.onClickDelete} />
 			</td></tr>
