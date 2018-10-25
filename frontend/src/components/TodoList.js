b5c6cf79556c3dd12e8c7a520f87ca2dc8789a3a
Jeremy Brown <jeremy.r.brown@gmail.com>
2018-10-24 23:34:33 -0700

    Add bootstrap styling to TodoList

--- frontend/src/components/TodoList.js
+++ frontend/src/components/TodoList.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import axios from 'axios';
 import TodoItem from './TodoItem';
 import TodoCreate from './TodoCreate';
+import {BrowserRouter as Router} from 'react-router-dom';
+import {Grid, Col, Row, Panel, Table} from 'react-bootstrap';
 
 class TodoList extends Component
 {
@@ -13,6 +15,7 @@ class TodoList extends Component
 			items: []
 		};
 		this.itemAdded = this.itemAdded.bind(this);
+		this.itemDeleted = this.itemDeleted.bind(this);
 	}
 	
 	componentDidMount()
@@ -26,25 +29,50 @@ class TodoList extends Component
 	
 	itemAdded(newItem)
 	{
+		//Add the new element to the list
 		let {items} = this.state;
 		items.push(newItem);
 		this.setState({items: items});
+	}
+	
+	itemDeleted(item)
+	{
+		//Find the element and delete it from the list
+		let {items} = this.state;
+		let index = items.findIndex((it) => {
+			return it.id === item.id;
+		});
+		if(index > -1)
+		{
+			items.splice(index, 1);
+		}
+		this.setState({items: items});
+		
+		//TODO: another way would be just to reload the list
 	}
 	
 	render()
 	{
 		return (
 			<div>
-				<h2>To-Do List</h2>
-				<table>
-					{
-						this.state.items.map(t =>
-							<TodoItem text={t.text} completed={t.completed} />
-						)
-					}
-				</table>
-				<br/>
-				<TodoCreate onAdd={this.itemAdded} />
+				<Grid><Row><Col xs={12} sm={8} md={6} lg={4}>
+				<Panel>
+					<Panel.Heading>
+						<Panel.Title componentClass="h3">To-Do List</Panel.Title>
+					</Panel.Heading>
+					<Panel.Body>
+						<Table striped>
+							{
+								this.state.items.map(t =>
+									<TodoItem todo={t} onDelete={this.itemDeleted} />
+								)
+							}
+							</Table>
+						<br/>
+						<TodoCreate onAdd={this.itemAdded} />
+					</Panel.Body>
+				</Panel>
+				</Col></Row></Grid>
 			</div>
 		);
 	}
